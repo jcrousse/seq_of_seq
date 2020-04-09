c95b770e57f61a3155c9be797410a13c33c54dca
John Crousse <jcrousse@gmail.com>
2020-04-09 10:18:01 +0200

    TF sequence model.

--- models/simple_sequence/sequence_model.py
+++ models/simple_sequence/sequence_model.py
@@ -2,8 +2,7 @@ import tensorflow as tf
 
 from data_reader import DataReader
 
-
-# todo now: tensorboard output because it is nicer. Clean up this crap.
+# todo now: tensorboard output. Comparison with GRU / LSTM
 n_test = 10000
 n_train = 5000
 n_val = 2000
@@ -13,46 +12,26 @@ max_len = 200
 test_texts, test_labels = DataReader(0.5, dataset='test').take(n_test)
 
 train_dr = DataReader(0.5)
-train_texts, labels = train_dr.take(n_train)
+train_texts, train_labels = train_dr.take(n_train)
 val_texts, val_labels = train_dr.take(n_val)
 
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size,
-                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
-tokenizer.fit_on_texts(train_texts)
+tokenizer_train = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size,
+                                                        filters=r'!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+tokenizer_train.fit_on_texts(train_texts)
 
-train_tokens = tokenizer.texts_to_sequences(train_texts)
-test_tokens = tokenizer.texts_to_sequences(test_texts)
-val_tokens = tokenizer.texts_to_sequences(val_texts)
 
-pad_seq_train = tf.keras.preprocessing.sequence.pad_sequences(train_tokens, padding='post', maxlen=max_len)
-pad_seq_val = tf.keras.preprocessing.sequence.pad_sequences(val_tokens, padding='post', maxlen=max_len)
-pad_seq_test = tf.keras.preprocessing.sequence.pad_sequences(test_tokens, padding='post', maxlen=max_len)
+def get_dataset(texts, labels, tokenizer, batch_size=256, seq_len=max_len):
+    tokens = tokenizer.texts_to_sequences(texts)
+    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(tokens, padding='post', maxlen=seq_len)
+    dataset = tf.data.Dataset.from_tensor_slices((padded_sequences, labels))
+    batches = (dataset.shuffle(1000).padded_batch(batch_size, padded_shapes=([None], [])))
+    return batches
 
-print("max length:", max(len(t) for t in pad_seq_train))
 
-dataset_train = tf.data.Dataset.from_tensor_slices((pad_seq_train, labels))
-dataset_val = tf.data.Dataset.from_tensor_slices((pad_seq_val, val_labels))
-dataset_test = tf.data.Dataset.from_tensor_slices((pad_seq_test, test_labels))
-train_batches = (
-    dataset_train
-    .shuffle(1000)
-    .padded_batch(256, padded_shapes=([None], [])))
+train_batches = get_dataset(train_texts, train_labels, tokenizer_train)
+validation_batches = get_dataset(val_texts, val_labels, tokenizer_train)
+test_batches = get_dataset(test_texts, test_labels, tokenizer_train)
 
-validation_batches = (
-    dataset_val
-    .shuffle(1000)
-    .padded_batch(256, padded_shapes=([None], [])))
-
-test_batches = (
-    dataset_test
-    .shuffle(1000)
-    .padded_batch(256, padded_shapes=([None], [])))
-
-for example_batch, label_batch in dataset_train.take(2):
-    print(example_batch)
-    print(label_batch)
-    print("Batch shape:", example_batch.shape)
-    print("label shape:", label_batch.shape)
 
 model = tf.keras.Sequential([
   tf.keras.layers.Embedding(vocab_size, 16),
@@ -72,4 +51,4 @@ history = model.fit(train_batches,
 results = model.evaluate(test_batches, verbose=2)
 
 for name, value in zip(model.metrics_names, results):
-  print("%s: %.3f" % (name, value))
+    print("%s: %.3f" % (name, value))
